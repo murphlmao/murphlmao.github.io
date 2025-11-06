57cc12d199c02e3727570cf50ad45fdd6100c228
murphlmao <murphyjmalcolm@gmail.com>
2025-11-06 00:45:01 -0500

    fix: weird spacing date issue

--- src/app/blog/BlogPagination.js
+++ src/app/blog/BlogPagination.js
@@ -20,7 +20,7 @@ export default function BlogPagination({ articles, articlesPerPage }) {
 
   return (
     <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-      <div className="flex max-w-3xl flex-col space-y-16">
+      <div className="flex max-w-2xl flex-col space-y-16">
         {currentArticles.map((article) => (
           <article
             key={article.slug}
